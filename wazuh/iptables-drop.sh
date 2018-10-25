4d480db487086ec14e6cb231fcf2deb98b47b63b
mitchya1 <mitchell.anderson@colorado.edu>
2018-10-25 16:24:01 -0600

    ready to go

---
+++ wazuh/iptables-drop.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+LOCK_FILE="IPTABLES_LOCK"
+LOG_FILE="/var/ossec/logs/active-responses.log"
+ACTION=$1
+IP=$3
+check_lock_file () {
+DATE="$(date)"
+if [ -f "${LOCK_FILE}" ]
+then
+    echo "LOCKED"
+    echo "${DATE} Locked" >> ${LOG_FILE}
+    sleep 1
+    # Try again
+    check_lock_file
+else
+    # Proceed
+    firewall
+fi
+}
+
+firewall () {
+DATE="$(date)"
+touch "${LOCK_FILE}"
+echo "${DATE} $0 ${ACTION} ${2} ${IP}" >> ${LOG_FILE}
+if [ "${ACTION}" == "add" ]
+then
+    sudo iptables -I INPUT -s ${IP} -j DROP
+else
+    sudo iptables -D INPUT -s ${IP} -j DROP
+rm "${LOCK_FILE}"
+exit 0
+fi
+}
+check_lock_file
