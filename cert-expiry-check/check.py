99fc93468f8c03520cf95261daee9df38c25f05b
mitchya1 <mitchell.anderson@colorado.edu>
2019-05-02 10:07:39 -0600

    add try statement, move config to config module

--- cert-expiry-check/check.py
+++ cert-expiry-check/check.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
 # Forked from https://serverlesscode.com/post/ssl-expiration-alerts-with-lambda/
 import sys, datetime, ssl, socket, json
+from config import HOSTS, ALERT_TIME, CHANNEL, USERNAME, EMOJI, INCOMING_WEBHOOK
 try:
     import requests
 except:
     print("It doesn't seem like you've installed requests")
     print("Try pip install --requests")
-
-# Hosts to monitor
-HOSTS = ['imm.com', 'boulderheavyindustries.com', 'factandfiction.work', 'respondology.com', 'visiqua.com']
-# How many days prior to cert expiration should we alert?
-ALERT_TIME = 10
-
-# Slack stuff
-INCOMING_WEBHOOK = ""
-USERNAME = "TLS Certificate Checker"
-CHANNEL = "#cert-alerts"
-EMOJI = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRihczVMjBKJqN7uFHZRTcwcsq1M-3ulBsfgb-5ZqMaLavLsEnYsA"
-
 
 # If running on a Mac, follow the steps in this answer: https://stackoverflow.com/questions/41691327/ssl-sslerror-ssl-certificate-verify-failed-certificate-verify-failed-ssl-c/41692664
 def ssl_expiry_datetime():
@@ -29,17 +18,21 @@ def ssl_expiry_datetime():
             socket.socket(socket.AF_INET),
             server_hostname=HOST,
         )
-        # 3 second timeout because Lambda has runtime limitations
+        # 3 second timeout because we don't want to wait around forever, feel free to change this
         conn.settimeout(3.0)
-        conn.connect((HOST, 443))
-        ssl_info = conn.getpeercert()
-        # parse the string from the certificate into a Python datetime object
-        expire_date = datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
-        remaining_lifetime = expire_date - datetime.datetime.utcnow()
-        if remaining_lifetime < datetime.timedelta(days=ALERT_TIME):
-            slack_alert(HOST, remaining_lifetime)
-        else:
-            continue
+        # If we don't use a try block, an expired cert or unreachable host will tank the script
+        try:
+            conn.connect((HOST, 443))
+            ssl_info = conn.getpeercert()
+            # parse the string from the certificate into a Python datetime object
+            expire_date = datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+            remaining_lifetime = expire_date - datetime.datetime.utcnow()
+            if remaining_lifetime < datetime.timedelta(days=ALERT_TIME):
+                slack_alert(HOST, remaining_lifetime)
+            else:
+                continue
+        except Exception as e:
+            print(f"Hit an exception checking {HOST}:\n {str(e)}")
 
 
 
---
+++ cert-expiry-check/config.py
@@ -0,0 +1,11 @@
+# Hosts to monitor
+HOSTS = ['imm.com', 'boulderheavyindustries.com', 'factandfiction.work', 'respondology.com', 'visiqua.com']
+
+# How many days prior to cert expiration should we alert?
+ALERT_TIME = 10
+
+# Slack stuff
+INCOMING_WEBHOOK = ""
+USERNAME = "TLS Certificate Checker"
+CHANNEL = "#cert-alerts"
+EMOJI = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRihczVMjBKJqN7uFHZRTcwcsq1M-3ulBsfgb-5ZqMaLavLsEnYsA"
